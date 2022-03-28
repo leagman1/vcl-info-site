69fb22eb3088a8f231b4465e94312b2bc3eb06f3
leagman1 <76821252+leagman1@users.noreply.github.com>
2022-03-29 00:45:44 +0200

    Map pool draft

--- app.js
+++ app.js
@@ -3,14 +3,32 @@ const path = require("path");
 const app = express();
 const port = 8080;
 
+const fs = require("fs");
+
 app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+var mapFile = fs.readFileSync("data/maps.json");
+var mapPool = JSON.parse(mapFile);
+console.log(mapPool);
+
+var seasons = [
+    {
+        id: 1
+    },
+    {
+        id: 2
+    },
+    {
+        id: 3
+    }
+];
+
 app.get('/', (req, res) => {
-  res.render('maps')
+  res.render('maps', {seasons: seasons, mapPool: mapPool});
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
