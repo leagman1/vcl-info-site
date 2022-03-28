3239a8b03467a18eada2c2822cfd91417e05dba5
leagman1 <76821252+leagman1@users.noreply.github.com>
2022-03-28 21:11:09 +0200

    Prep prototyping

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 const port = 8080;
 
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.render('base_banner')
+  res.render('maps')
 })
 
 app.listen(port, () => {
